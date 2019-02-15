Fixed numerical instability, but still have weird behavior during training
The issue was the variance of the random normal terms used during
initialization. I had been using the same variance as in the earlier
(working) version of this MPS, but for some reason that led to
instability in this version. The model now actually learns from
data and doesn't explode during training, although...

The MPS module currently improves its performance much more quickly in
the presence of periodic boundary conditions. Choosing either open
boundary conditions or dynamic bond dimensions seems to freeze the
model parameters, which doesn't make any sense. More probing is
needed to figure out what is causing this behavior, both by
experimenting with the hyperparameters and by digging into the
internals more during training.

contractables.py
@@ -245,12 +245,12 @@ class MatRegion(Contractable):
 
         super().__init__(mats, bond_str='bslr')
 
-    def __mul__(self, right_vec):
+    def __mul__(self, edge_vec, rmul=False):
         """
-        Iteratively multiply an input vector on the left with all our matrices
+        Iteratively multiply an input vector with all matrices in MatRegion
         """
         # The input must be an instance of EdgeVec
-        if not isinstance(right_vec, EdgeVec):
+        if not isinstance(edge_vec, EdgeVec):
             return NotImplemented
 
         mats = self.tensor
@@ -258,38 +258,36 @@ class MatRegion(Contractable):
         batch_size = mats.size(0)
 
         # Load our vector and matrix batches
-        vec = right_vec.tensor.unsqueeze(2)
+        dummy_ind = 1 if rmul else 2
+        vec = edge_vec.tensor.unsqueeze(dummy_ind)
         mat_list = [mat.squeeze(1) for mat in torch.chunk(mats, num_mats, 1)]
 
-        # Do the repeated matrix-vector multiplications in right-to-left order
-        for mat in mat_list[::-1]:
-            vec = torch.bmm(mat, vec)
+        # Do the repeated matrix-vector multiplications in the proper order
+        log_norm = 0
+        for i, mat in enumerate(mat_list[::(1 if rmul else -1)], 1):
+            if rmul:
+                vec = torch.bmm(vec, mat)
+            else:
+                vec = torch.bmm(mat, vec)
+
+            # Rescale our vectors to keep the average norm equal to 1
+            # if i % 10 == 0:
+            #     # with torch.no_grad():
+            #     av_norm = sum([torch.norm(v) for v in vec]) / batch_size
+            #     log_norm += torch.log(av_norm)
+            #     vec = vec / av_norm
+
+        # If our normalization isn't too huge (~1e34), then reapply it to vec
+        # if abs(log_norm) < 80:
+        # vec = vec * torch.exp(log_norm)
+        # else:
+        #     print(f"Implicit mult by exp({-log_norm}) to keep normalization")
 
         # Since we only have a single vector, wrap it as a EdgeVec
-        return EdgeVec(vec.squeeze(2), is_left_vec=False)
+        return EdgeVec(vec.squeeze(dummy_ind), is_left_vec=rmul)
 
-    def __rmul__(self, left_vec):
-        """
-        Iteratively multiply an input vector on the right with all our matrices 
-        """
-        # The input must be an instance of EdgeVec
-        if not isinstance(left_vec, EdgeVec):
-            return NotImplemented
-
-        mats = self.tensor
-        num_mats = mats.size(1)
-        batch_size = mats.size(0)
-
-        # Load our vector and matrix batches
-        vec = left_vec.tensor.unsqueeze(1)
-        mat_list = [mat.squeeze(1) for mat in torch.chunk(mats, num_mats, 1)]
-
-        # Do the repeated matrix-vector multiplications in right-to-left order
-        for mat in mat_list:
-            vec = torch.bmm(vec, mat)
-
-        # Since we only have a single vector, wrap it as a EdgeVec
-        return EdgeVec(vec.squeeze(1), is_left_vec=True)
+    def __rmul__(self, edge_vec):
+        return self.__mul__(edge_vec, rmul=True)
 
     def reduce(self):
         """

cores.py
@@ -90,13 +90,14 @@ class MPS(nn.Module):
         """
         Embed our data and pass it to an MPS with a single output site
         """
-
         # WHEN IMPLEMENTING ROUTING FOR CUSTOM PATHS, THAT CODE GOES HERE
 
         # Embed our input data before feeding it into our linear region
         input_data = self.embed_input(input_data)
+        output = self.linear_region(input_data)
 
-        return self.linear_region(input_data)
+        # return output
+        return torch.abs(output)
 
 class LinearRegion(nn.Module):
     """
@@ -211,7 +212,7 @@ class MergedLinearRegion(LinearRegion):
                  cutoff=1e-10, threshold=1000):
         # Initialize a LinearRegion with our given module_list
         super().__init__(module_list, periodic_bc, parallel_eval)
-
+        
         # Merge all of our parameter tensors, which rewrites self.module_list
         self.offset = 0
         self.merge(offset=self.offset)
@@ -433,7 +434,7 @@ class InputRegion(nn.Module):
             tensor = init_tensor(shape, bond_str, 'random_eye')
 
         # Register our tensor as a Pytorch Parameter
-        self.tensor = nn.Parameter(tensor)
+        self.tensor = nn.Parameter(tensor.contiguous())
 
     def forward(self, input_data):
         """
@@ -527,7 +528,7 @@ class MergedInput(nn.Module):
         super().__init__()
 
         # Register our tensor as a Pytorch Parameter
-        self.tensor = nn.Parameter(tensor)
+        self.tensor = nn.Parameter(tensor.contiguous())
 
     def forward(self, input_data):
         """
@@ -602,7 +603,7 @@ class InputSite(nn.Module):
             tensor = init_tensor(shape, bond_str, 'random_eye')
 
         # Register our tensor as a Pytorch Parameter
-        self.tensor = nn.Parameter(tensor)
+        self.tensor = nn.Parameter(tensor.contiguous())
 
     def forward(self, input_data):
         """
@@ -641,7 +642,7 @@ class OutputSite(nn.Module):
             tensor = init_tensor(shape, bond_str, 'random_eye')
 
         # Register our tensor as a Pytorch Parameter
-        self.tensor = nn.Parameter(tensor)
+        self.tensor = nn.Parameter(tensor.contiguous())
 
     def forward(self, input_data):
         """
@@ -674,7 +675,7 @@ class MergedOutput(nn.Module):
         super().__init__()
 
         # Register our tensor as a Pytorch Parameter
-        self.tensor = nn.Parameter(tensor)
+        self.tensor = nn.Parameter(tensor.contiguous())
         self.left_output = left_output
 
     def forward(self, input_data):

train_script.py
@@ -5,21 +5,29 @@ from cores import MPS
 from torchvision import transforms, datasets
 
 # MPS parameters
-num_pixels = 28**2
 bond_dim = 10
-num_labels = 10
 dynamic_mode = False
-periodic_bc = False
+periodic_bc = True
 
 # Training parameters
-num_train = 1000
+num_train = 2000
 num_test = 1000
 batch_size = 100
-num_epochs = 10
+num_epochs = 15
+lr = 1e-3
+l2_reg = 0.
+
+# Initialize the MPS module
+mps = MPS(input_dim=28**2, output_dim=10, bond_dim=bond_dim, 
+          dynamic_mode=dynamic_mode, periodic_bc=periodic_bc)
+
+# Set loss function and optimizer
+loss_fun = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(mps.parameters(), lr=lr, weight_decay=l2_reg)
 
 # Miscellaneous initialization
 torch.set_default_tensor_type('torch.FloatTensor')
-torch.manual_seed(23)
+torch.manual_seed(0)
 start_time = time.time()
 
 # Get the training and test sets
@@ -37,16 +45,14 @@ loaders = {name: torch.utils.data.DataLoader(dataset, batch_size=batch_size,
 num_batches = {name: total_num // batch_size for (name, total_num) in
                [('train', num_train), ('test', num_test)]}
 
-# Initialize our MPS module
-mps = MPS(input_dim=num_pixels, output_dim=num_labels, bond_dim=bond_dim, 
-          dynamic_mode=dynamic_mode, periodic_bc=periodic_bc)
-
-# Set loss function and optimizer
-loss_fun = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(mps.parameters(), lr=1E-3)
-
-print(f"Training on {num_train} MNIST images for {num_epochs} epochs")
-print(f"Maximum MPS bond dimension = {bond_dim}\n")
+print(f"Training on {num_train} MNIST images \n"
+      f"(Testing on {num_test}) for {num_epochs} epochs")
+print(f"Maximum MPS bond dimension  = {bond_dim}")
+print(f"Using Adam w/ learning rate = {lr:.1e}")
+if l2_reg > 0:
+    print(f"    and L2 regularization   = {l2_reg:.2e}")
+print(f"{'Periodic' if periodic_bc else 'Open'} boundary conditions")
+print(f"{'Dynamic' if dynamic_mode else 'Static'} training mode\n")
 
 # Let's start training!
 for epoch_num in range(1, num_epochs+1):
@@ -54,7 +60,7 @@ for epoch_num in range(1, num_epochs+1):
     running_acc = 0.
 
     for inputs, labels in loaders['train']:
-        inputs, labels = inputs.view([batch_size, num_pixels]), labels.data
+        inputs, labels = inputs.view([batch_size, 28**2]), labels.data
 
         # Call our MPS to get logit scores and predictions
         scores = mps(inputs)
@@ -62,8 +68,8 @@ for epoch_num in range(1, num_epochs+1):
 
         # Compute the loss and accuracy, add them to the running totals
         loss = loss_fun(scores, labels)
-        accuracy = torch.sum(preds == labels) / batch_size
         with torch.no_grad():
+            accuracy = torch.sum(preds == labels).item() / batch_size
             running_loss += loss
             running_acc += accuracy
 
@@ -73,20 +79,20 @@ for epoch_num in range(1, num_epochs+1):
         optimizer.step()
 
     print(f"### Epoch {epoch_num} ###")
-    print(f"Average loss:           {running_loss / num_batches['train']}")
-    print(f"Average train accuracy: {running_acc / num_batches['train']}")
+    print(f"Average loss:           {running_loss / num_batches['train']:.3f}")
+    print(f"Average train accuracy: {running_acc / num_batches['train']:.3f}")
 
     # Evaluate accuracy of MPS classifier on the test set
     with torch.no_grad():
         running_acc = 0.
 
         for inputs, labels in loaders['test']:
-            inputs, labels = inputs.view([batch_size, num_pixels]), labels.data
+            inputs, labels = inputs.view([batch_size, 28**2]), labels.data
 
             # Call our MPS to get logit scores and predictions
             scores = mps(inputs)
             _, preds = torch.max(scores, 1)
-            running_acc += torch.sum(preds == labels) / batch_size
+            running_acc += torch.sum(preds == labels).item() / batch_size
 
-    print(f"Test accuracy:          {running_acc / num_batches['test']}")
+    print(f"Test accuracy:          {running_acc / num_batches['test']:.3f}")
     print(f"Runtime so far:         {int(time.time()-start_time)} sec\n")