Fixed issue with negligible gradients in training script

torchmps.py
@@ -303,7 +303,7 @@ class MPS(nn.Module):
         * Clean up the current treatment of initialization
         * Resolve weirdness with fixed bias and initialization choice
         * Add function to convert to canonical form
-        * Figure out why model isn't training currently
+        * Fix issue of no training when use_bias=False  
     """
     def __init__(self, input_dim, output_dim, bond_dim, feature_dim=2,
                  periodic_bc=False, parallel_eval=False, label_site=None, 

train_script.py
@@ -23,8 +23,7 @@ l2_reg     = 0.
 
 # Initialize the MPS module
 mps = MPS(input_dim=28**2, output_dim=10, bond_dim=bond_dim, 
-          adaptive_mode=adaptive_mode, periodic_bc=periodic_bc, 
-          use_bias=False)
+          adaptive_mode=adaptive_mode, periodic_bc=periodic_bc)
 
 # Set our loss function and optimizer
 loss_fun = torch.nn.CrossEntropyLoss()