0acaee7e9655806241f3df8d15df8a86d6a3a2be
Jacob Miller <jmjacobmiller@gmail.com>
2021-03-26 23:52:27 -0400

    Add setup.py and associated metadata for integration with pypi

--- torchmps/utils2.py
+++ torchmps/utils2.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Jacob Ellwyn Miller
+# Copyright (C) 2021 Jacob Miller
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
