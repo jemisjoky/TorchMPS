b9caaa30dd02bf7591151fcce76db52be1b92e30
Jacob Miller <jmjacobmiller@gmail.com>
2021-04-13 01:19:17 -0400

    Finished implementation of sequential contraction method

--- torchmps/mps_base.py
+++ torchmps/mps_base.py
@@ -26,16 +26,17 @@ TensorSeq = Union[Tensor, Sequence[Tensor]]
 
 def contract_matseq(
     matrices: TensorSeq,
-    bnd_vecs: Tuple[Optional[Tensor], Optional[Tensor]] = (None, None),
+    left_vec: Optional[Tensor] = None,
+    right_vec: Optional[Tensor] = None,
     parallel_eval: bool = False,
 ) -> Tensor:
     """
-    Batch matrix-multiply sequence of matrices, with optional boundary vectors
+    Matrix-multiply sequence of matrices with optional boundary vectors
 
-    The output is a single matrix, or a scalar if a pair of boundary
+    The output is a single matrix, or a vector/scalar if one/both boundary
     vectors are given. In the latter case, the first vector is treated as
-    a row vector and the last as a column vector and multiplied at the
-    beginning and end of the sequence of matrices to reduce to a scalar.
+    a row vector and the last as a column vector and put at the beginning
+    or end of the sequence of matrices to reduce to a vector/scalar.
 
     Parallel matrix-matrix multiplications can be used to make the
     computation more GPU-friendly, at the cost of a larger overall compute
@@ -43,7 +44,7 @@ def contract_matseq(
     desired, but can be forced by setting parallel_eval to True.
 
     When matrices or boundary vectors contain additional batch indices
-    (assumed to be left-most indices), then matrix multiplication is
+    (assumed to be left-most indices), then batch matrix multiplication is
     carried out over all batch indices, which are broadcast together.
     Shapes described below neglect these additional batch indices.
 
@@ -51,9 +52,10 @@ def contract_matseq(
         matrices: Single tensor of shape (L, D, D), or sequence of
             matrices with compatible shapes :math:`(D_i, D_{i+1})`, for
             :math:`i = 0, 1, \ldots, L`.
-        bnd_vecs: Pair of vectors `(l_vec, r_vec)` to place on the
-            boundaries of contracted matrices. Either value can be None,
-            in which case that matrix index is left open.
+        left_vec: Left boundary vector with shape `(D_0,)`, or None if no
+            left boundary is present.
+        right_vec: Left boundary vector with shape `(D_L,)`, or None if no
+            right boundary is present.
         parallel_eval: Whether or not to force parallel evaluation in
             matrix contraction, which requires all input matrices to have
             same shape.
@@ -64,8 +66,9 @@ def contract_matseq(
             two, one, or zero boundary vectors.
     """
     # Count number of boundary vectors
-    bnd_vecs = list(bnd_vecs)
-    num_vecs = sum(v is not None for v in bnd_vecs)
+    bnd_vecs = [left_vec, right_vec]
+    real_vec = [v is not None for v in bnd_vecs]
+    num_vecs = sum(real_vec)
     assert all(v is None or isinstance(v, Tensor) for v in bnd_vecs)
     assert num_vecs <= 2
 
@@ -82,7 +85,7 @@ def contract_matseq(
         if not same_shape:
             matrices = batch_broadcast(matrices, (2,) * num_mats)
     elif num_vecs == 1:
-        v_ind = [v is not None for v in bnd_vecs].index(True)
+        v_ind = real_vec.index(True)
         vec = bnd_vecs[v_ind]
         if same_shape:
             vec, matrices = batch_broadcast((vec, matrices), (1, 3))
@@ -103,25 +106,39 @@ def contract_matseq(
         product = mat_reduce_par(matrices)
 
         # Contract with boundary vectors
-        if bnd_vecs[1] is not None:
+        if real_vec[1]:
             product = torch.matmul(product, bnd_vecs[1][..., None])
-            product = product[..., 0]
-        if bnd_vecs[0] is not None:
+            product.squeeze_(-1)
+        if real_vec[0]:
             product = torch.matmul(bnd_vecs[0][..., None, :], product)
-            product = product[..., 0, :]
-
-        return product
+            product.squeeze_(-2)
 
     else:
-        if num_vecs == 0:
-            return matvec_reduce_seq(matrices)
-        elif num_vecs == 1:
-            pass
+        if num_vecs == 0 and len(matrices) == 0:
+            raise ValueError(
+                "Must input at least one matrix or boundary vector to contract_matseq"
+            )
+
+        # Prepend/append boundary vectors, augmented with dummy dimensions
+        matrices = list(matrices)
+        if real_vec[0]:
+            matrices = [bnd_vecs[0][..., None, :]] + matrices
+        if real_vec[1]:
+            matrices.append(bnd_vecs[1][..., None])
+
+        # Compute product sequentially and strip away dummy dimensions
+        product = matvec_reduce_seq(matrices)
+        if real_vec[0]:
+            product.squeeze_(-2)
+        if real_vec[1]:
+            product.squeeze_(-1)
+        
+    return product
 
 
 def mat_reduce_par(matrices: Tensor) -> Tensor:
     """
-    Contract sequence of square matrices using parallel mat-mat multiplies
+    Contract sequence of square matrices with parallel mat-mat multiplies
 
     Args:
         matrices: Sequence of matrices to multiply.
@@ -129,7 +146,35 @@ def mat_reduce_par(matrices: Tensor) -> Tensor:
     Returns:
         prod_mat: Product of input matrices
     """
-    pass
+    assert matrices.ndim >= 3
+    assert matrices.shape[-2] == matrices.shape[-1]
+    s_dim = -3  # Dimension which has spatial arrangement of matrices
+    n_mats = matrices.shape[s_dim]
+    num_batch = matrices.ndim - 3
+
+    # In case of empty collection of matrices, return the identity
+    if n_mats == 0:
+        eye = torch.eye(matrices.shape[-1], dtype=matrices.dtype)
+        matrices, _ = batch_broadcast((eye, matrices), (2, 3))
+        return matrices
+
+    # Iteratively multiply pairs of matrices until there is only one left
+    while n_mats > 1:
+        odd_n = n_mats % 2 == 1
+        half_n = n_mats // 2
+        floor_n = half_n * 2
+
+        # Split matrices up into even and odd numbers (maybe w/ leftover)
+        even_mats = matrices[..., 0:floor_n:2, :, :]
+        odd_mats = matrices[..., 1:floor_n:2, :, :]
+        leftover = matrices[..., floor_n:, :, :]
+
+        # Batch multiply everything, append remainder
+        matrices = even_mats @ odd_mats
+        matrices = torch.cat((matrices, leftover), dim=s_dim)
+        n_mats = matrices.shape[s_dim]
+
+    return matrices.squeeze(dim=s_dim)
 
 
 def mat_reduce_seq(matrices: Sequence[Tensor]) -> Tensor:
@@ -142,7 +187,16 @@ def mat_reduce_seq(matrices: Sequence[Tensor]) -> Tensor:
     Returns:
         prod_mat: Product of input matrices
     """
-    prod_mat, matrices = matrices[0], matrices[1:]
+    # Multiplication from left to right, so flip matrices if it's cheaper
+    # to multiply from right to left
+    r2l = matrices[-1].size(-1) < matrices[0].size(-2)
+    if r2l:
+        matrices = tuple(m.transpose(-2, -1) for m in matrices[::-1])
 
+    # Multiply all matrices sequentially, from left to right
+    product, matrices = matrices[0], matrices[1:]
     for mat in matrices:
-        pass
+        product = torch.matmul(product, mat)
+
+    # Revert to original form before returning
+    return product.transpose(-2, -1) if r2l else product
