Fixed normalization issue coming from assuming identity lamb_mat in frameification
Still need to replace the use of Cholesky decomposition by something better.
Perhaps I could first try the Cholesky decomp, and if I catch a runtime error,
just call an eigendecomp-based method, where there is a cutoff on eigenvalues?

torchmps/embeddings.py
@@ -79,10 +79,11 @@ class FixedEmbedding(nn.Module):
             the data fed to the embedding function is defined.
         frameify (bool): Whether to rescale embedding vectors to make the
             embedding function into a frame. (Default: False)
+        dtype: Datatype to save associated lambda matrix in. (Default: float32)
     """
 
     def __init__(
-        self, emb_fun: Callable, data_domain: DataDomain, frameify: bool = False
+        self, emb_fun: Callable, data_domain: DataDomain, frameify: bool = False, dtype: torch.dtype = torch.float32
     ):
         super().__init__()
         assert hasattr(emb_fun, "__call__")
@@ -91,6 +92,7 @@ class FixedEmbedding(nn.Module):
         self.domain = data_domain
         self.frameify = frameify
         self.emb_fun = emb_fun
+        self.dtype = dtype
         self.make_lambda()
 
         # Compute correcting "skew matrix" for frameified embedding
@@ -117,13 +119,14 @@ class FixedEmbedding(nn.Module):
             lamb_mat = self.raw_lamb_mat.double()
 
         # Skew mat is pseudoinverse of the (adjoint of) Cholesky factor of lambda mat
+        # TODO: Replace this with something using eigenvalue factorization
         cholesky_factor = torch.linalg.cholesky(lamb_mat).T.conj()
         skew_mat = torch.linalg.pinv(cholesky_factor)
+
+        # Unskew lambda matrix, then save matrices as initial dtypes
+        self.lamb_mat = (skew_mat.T @ lamb_mat @ skew_mat).to(original_dtype)
         self.skew_mat = skew_mat.to(original_dtype)
-
-        # Use of skew matrix changes lamb_mat to the identity
-        self.lamb_mat = torch.ones(())
-
+        
     @torch.no_grad()
     def make_lambda(self, num_points: int = 1000, shrink_mat: bool = True):
         """
@@ -167,6 +170,7 @@ class FixedEmbedding(nn.Module):
                 lamb_mat = lamb_mat
 
         # For unframeified embeddings, use as regular lamb_mat
+        lamb_mat = lamb_mat.to(self.dtype)
         self.raw_lamb_mat = lamb_mat
         if not self.frameify:
             self.lamb_mat = self.raw_lamb_mat
@@ -184,7 +188,6 @@ class FixedEmbedding(nn.Module):
             emb_vecs = emb_vecs[..., None, :]
             skew_mat = self.skew_mat[(None,) * num_batch_dims]
             emb_vecs = torch.matmul(emb_vecs, skew_mat)
-            # emb_vecs = torch.matmul(skew_mat, emb_vecs)
 
             # Remove extra singleton dimension
             assert emb_vecs.size(-2) == 1
@@ -244,12 +247,13 @@ class TrainableEmbedding(nn.Module):
             lamb_mat = self.raw_lamb_mat.double()
 
         # Skew mat is pseudoinverse of the (adjoint of) Cholesky factor of lambda mat
+        # TODO: Replace this with something using eigenvalue factorization
         cholesky_factor = torch.linalg.cholesky(lamb_mat).T.conj()
         skew_mat = torch.linalg.pinv(cholesky_factor)
-        self.skew_mat = skew_mat.to(original_dtype)
 
-        # Use of skew matrix changes lamb_mat to the identity
-        self.lamb_mat = torch.ones(())
+        # Unskew lambda matrix, then save matrices as initial dtypes
+        self.lamb_mat = (skew_mat.T @ lamb_mat @ skew_mat).to(original_dtype)
+        self.skew_mat = skew_mat.to(original_dtype)
 
     def make_lambda(self, num_points: int = 100, shrink_mat: bool = True):
         """

torchmps/prob_mps.py
@@ -171,6 +171,10 @@ class ProbMPS(nn.Module):
         if self.embedding is not None:
             input_data = self.embedding(input_data)
 
+        # If input isn't integral, convert it to same dtype as core tensors
+        if input_data.is_floating_point() or input_data.is_complex():
+            input_data = input_data.to(self.core_tensors.dtype)
+
         if slim_eval:
             if self.use_bias:
                 raise ValueError("Bias matrices not supported for slim_eval")
@@ -250,7 +254,10 @@ class ProbMPS(nn.Module):
             core_tensors = self.core_tensors
 
         # Account for non-trivial lambda function in the embedding
-        lamb_mat = None if self.embedding is None else self.embedding.lamb_mat
+        if self.embedding is None:
+            lamb_mat = None
+        else:
+            lamb_mat = self.embedding.lamb_mat.to(self.core_tensors.dtype)
 
         return get_log_norm(core_tensors, self.edge_vecs, lamb_mat=lamb_mat)
 
@@ -453,7 +460,10 @@ class ProbUnifMPS(ProbMPS):
             core_tensors = self.core_tensors
 
         # Account for non-trivial lambda function in the embedding
-        lamb_mat = None if self.embedding is None else self.embedding.lamb_mat
+        if self.embedding is None:
+            lamb_mat = None
+        else:
+            lamb_mat = self.embedding.lamb_mat.to(self.core_tensors.dtype)
 
         return get_log_norm(
             core_tensors, self.edge_vecs, lamb_mat=lamb_mat, length=data_len

torchmps/tests/test_embeddings.py
@@ -25,7 +25,7 @@ from functools import partial
 from itertools import product
 
 import torch
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, strategies as st
 
 from torchmps import ProbMPS
 from torchmps.embeddings import (
@@ -122,18 +122,13 @@ def test_frameified_embeddings(raw_embed, input_dim):
     # Verify the lambda matrix for framified embedding is identity
     assert allcloseish(lamb_mat, torch.eye(input_dim), tol=input_dim * 1e-2)
 
-    if raw_embed is legendre_embed:
-        breakpoint()
 
-
-@given(st.integers(1, 10))
+@given(st.integers(1, 50))
 def test_legendre_embedding(input_dim):
     """
     Verify that Legendre polynomial embedding is indeed a frame
     """
     embed_fun = partial(legendre_embed, emb_dim=input_dim)
-    # data_domain = DataDomain(continuous=True, min_val=0.0, max_val=1.0)
-    # fixed_embed = FixedEmbedding(embed_fun, data_domain, frameify=frameify)
 
     # Manually compute the lambda matrix for frameified embedding
     points = torch.linspace(0, 1, steps=1000)
@@ -148,14 +143,16 @@ def test_legendre_embedding(input_dim):
 @pytest.mark.parametrize(
     "raw_embed, frameify", list(product([trig_embed, legendre_embed], [False, True]))
 )
-@given(st.integers(1, 10), st.booleans())
+@settings(deadline=None)
+@given(st.integers(1, 15), st.booleans())
 def test_normalization(raw_embed, frameify, input_dim, complex_params):
     """
     For a 2-mode MPS, verify that probabilities integrate to 1
     """
+    torch.manual_seed(0)
     complex_params=False
     bond_dim = 10
-    num_points = 50
+    num_points = 200 if raw_embed is legendre_embed else 50
     embed_fun = FixedEmbedding(
         partial(raw_embed, emb_dim=input_dim), unit_interval, frameify=frameify
     )
@@ -177,17 +174,22 @@ def test_normalization(raw_embed, frameify, input_dim, complex_params):
     points = torch.linspace(0, 1, num_points)
     x, y = torch.meshgrid(points, points, indexing="ij")
     all_points = torch.stack((x, y), dim=-1)
-    dx = dy = 1 / (num_points - 1)
 
     # Feed flattened (x, y) pairs to MPS, then integrate over both axes
-    log_pdf = mps(all_points.reshape(-1, 2))
-    pdf = log_pdf.reshape(num_points, num_points).exp()
-    int_prob = torch.trapz(torch.trapz(pdf, dx=dy, dim=1), dx=dx, dim=0)
+    log_pdf = mps(all_points.reshape(-1, 2)).reshape(num_points, num_points)
+    pdf = log_pdf.exp()
+    int_prob = torch.trapz(torch.trapz(pdf, points, dim=1), points, dim=0)
 
     # try:
-    assert allcloseish(int_prob, torch.ones((), dtype=int_prob.dtype), tol=1e-2)
-    # except:
+    assert allcloseish(int_prob, torch.ones((), dtype=int_prob.dtype), tol=1e-1)
+    # except RuntimeError:
     #     breakpoint()
+    #     lamb_mat = mps.embedding.lamb_mat
+    #     raw_lamb_mat = mps.embedding.raw_lamb_mat
+    #     cholesky_factor = torch.linalg.cholesky(raw_lamb_mat.double()).T.conj()
+    #     skew_mat = torch.linalg.pinv(cholesky_factor)
+    #     print((skew_mat.T @ lamb_mat @ skew_mat - lamb_mat).norm())
+
 
 
 # # TODO: Get the following test actually working